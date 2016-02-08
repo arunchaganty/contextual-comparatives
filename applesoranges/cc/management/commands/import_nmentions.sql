406ccb8075d2c18728de62f4dbafe303d1075634
Arun Tejasvi Chaganty <arunchaganty@gmail.com>
2016-02-08 10:18:01 -0800

    Added import sql scripts

--- applesoranges/cc/management/commands/import_nmentions.sql
+++ applesoranges/cc/management/commands/import_nmentions.sql
@@ -1,2 +1,2 @@
 COPY cc_numericmention(value, unit, type, sentence_id, doc_char_begin, doc_char_end, token_begin, token_end)
-FROM '/home/chaganty/Research/contextual-comparatives/data/money_mentions.tsv' WITH CSV HEADER DELIMITER E'\t';
+FROM '/Users/chaganty/Research/contextual-comparatives/data/money_mentions.tsv' WITH CSV HEADER DELIMITER E'\t';
