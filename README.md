89348ed2e0820533200f1fa5e64a174de05e34da
Arun Tejasvi Chaganty <arunchaganty@gmail.com>
2016-06-12 22:18:07 -0700

    Updates to create a jar and reproduce table 5a

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # contextual-comparatives
 Putting numbers into context.
+
+Please see the codalab page for information about the paper and data.
+
+# Installation / Prerequisites
+
+The `pull-dependencies` script should set you up for all code dependencies you need, namely:
+
+* `CoreNLP` (Stanford's NLP library)
+* `junit` (for the test cases)
+* `hamcrest` (for the test cases)
+
+Just run it from the project checkout directory.
+
+    $> pull-dependencies
+
+You will also need to set the `JAVA_HOME` environment variable to
+point to your JDK installation.
+This project requires the JDK for Java 1.8.
+
+Once ready, run `ant` to build the project.
+
+    $> ant
+
+# Learning parameters.
+
+(type of training data required).
+(command).
+(script.)
+
---
+++ reproduce_table5a.sh
@@ -0,0 +1,36 @@
+# This script reproduces the results presented in Table 5(a) of the
+# paper, which evaluates the formula selection component.
+
+source util.lib
+
+TRAIN_DATA=data/rank_majority.tsv
+FEATURES=(
+  length-bias,numeric-proximity
+  length-bias,arg-ssimilarity
+  length-bias,fact-id
+  length-bias,fact-cross
+  length-bias,fact-id,fact-cross
+  length-bias,fact-id,fact-cross,numeric-proximity
+  length-bias,fact-id,fact-cross,numeric-proximity,arg-ssimilarity
+  );
+
+assert_exists $TRAIN_DATA
+
+# Create a header for the training results.
+echo "features        train-acc       train-prec      train-rec       train-f1        test-acc       test-prec        test-rec        test-f1" > table5a.tsv
+
+ensure_dir outputs
+ensure_dir models
+ensure_dir logs
+
+# For each of the features.
+for features in ${FEATURES[@]}; do
+  assert "-n $features" "Invalid feature: $features";
+  info "Learning model for $features";
+  # Run the learning procedure.
+  bin/run.sh edu.stanford.nlp.perspectives.LearnRanking -input data/rank_majority.tsv -embeddings data/word_embeddings.tsv -output models/$features.model -features $features -outputPredictions outputs/$features.tsv > logs/$features.log
+  # Process the output to fit into the tsv format.
+  tail -n 1 logs/$features.log >> table5a.tsv
+done;
+
+info "Table 5a ready!"
