b8e23f9b03844c2dd2f37ecf35ad47b77553a0ce
Arun Tejasvi Chaganty <chaganty@stanford.edu>
2016-06-13 16:13:53 -0700

    Updated dependencies and run script

--- bin/run.sh
+++ bin/run.sh
@@ -5,12 +5,15 @@ deps=
 for f in lib/*.jar; do
   deps=$f:$deps;
 done;
-for f in CoreNLP/*.jar; do
+for f in lib/CoreNLP/*.jar; do
   deps=$f:$deps;
 done;
-#for f in lib/stanford-corenlp-full-2015-12-09/*.jar; do
-#  deps=$f:$deps;
-#done;
+for f in lib/CoreNLP/lib/*.jar; do
+  deps=$f:$deps;
+done;
+for f in lib/CoreNLP/liblocal/*.jar; do
+  deps=$f:$deps;
+done;
 
-$JAVA_PATH/java -ea -Xms6g -Xmx6g -cp $deps:out/production/contextual-comparatives $@
+$JAVA_PATH/java -ea -Xms6g -Xmx6g -cp out/production/contextual-comparatives:$deps $@
 
